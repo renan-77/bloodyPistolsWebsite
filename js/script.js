2806c4f4cbe5d49162d0d074edf6ca450c63c1c8
renan-77 <renan.deoliveiramonteiro@student.griffith.ie>
2019-05-02 18:51:37 +0100

    Mobile Version Menu fixed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+function mobileMenu(){
 $('.navTrigger').click(function () {
+    $('.nav').css("background-color", "#16161696");
     $(this).toggleClass('active');
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
+});
+}
 
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
 });
